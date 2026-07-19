e952f8c43120059e3432429f5000a2aa69d12652
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-19 21:49:28 +0530

    Show tool name in history list

--- client/src/components/HistoryList.jsx
+++ client/src/components/HistoryList.jsx
@@ -5,6 +5,7 @@ export function HistoryList({ history }) {
     <div className="history-list">
       {history.map((log) => (
         <div key={log._id} className="history-row">
+          <span className="history-tool">{log.tool}</span>
           <span className="history-topic">{log.topic}</span>
           <span className="history-count">{log.resultCount} results</span>
           <span className="history-time">
